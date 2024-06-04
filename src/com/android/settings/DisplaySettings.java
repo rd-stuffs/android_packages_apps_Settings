9efabeef84714ca612e72d9e9cfb9c7588d38657
Richard Raya <rdxzv.dev@gmail.com>
2024-06-04 02:03:59 -0300

    Settings: Restore AOSP DT2W [2/2]
    Change-Id: Ia3ea0bc42c321234b3a5fce8b15588b86d49ceaf
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -28,6 +28,7 @@ import com.android.settings.display.LiftToWakePreferenceController;
 import com.android.settings.display.PocketJudgePreferenceController;
 import com.android.settings.display.ShowOperatorNamePreferenceController;
 import com.android.settings.display.ShowRefreshRatePreferenceController;
+import com.android.settings.display.TapToWakePreferenceController;
 import com.android.settings.display.ThemePreferenceController;
 import com.android.settings.display.VrDisplayPreferenceController;
 import com.android.settings.search.BaseSearchIndexProvider;
@@ -85,6 +86,7 @@ public class DisplaySettings extends DashboardFragment {
         controllers.add(new CameraGesturePreferenceController(context));
         controllers.add(new LiftToWakePreferenceController(context));
         controllers.add(new PocketJudgePreferenceController(context));
+        controllers.add(new TapToWakePreferenceController(context));
         controllers.add(new VrDisplayPreferenceController(context));
         controllers.add(new ShowOperatorNamePreferenceController(context));
         controllers.add(new ShowRefreshRatePreferenceController(context));

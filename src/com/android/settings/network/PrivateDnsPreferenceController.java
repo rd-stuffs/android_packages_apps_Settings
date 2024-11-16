ebc7006648359b325603bef70e18f4a1592de8f5
Richard Raya <rdxzv.dev@gmail.com>
2024-11-16 02:03:48 -0300

    Settings: Drop custom DNS options
    Change-Id: Ie693fe449eb52fcf90f4075a0a3aa04b5a64105b
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- src/com/android/settings/network/PrivateDnsPreferenceController.java
+++ src/com/android/settings/network/PrivateDnsPreferenceController.java
@@ -65,10 +65,6 @@ public class PrivateDnsPreferenceController extends BasePreferenceController
         Settings.Global.getUriFor(PRIVATE_DNS_SPECIFIER),
     };
 
-    // Only used in Settings, update on additions to ConnectivitySettingsUtils
-    private static final int PRIVATE_DNS_MODE_CLOUDFLARE = 4;
-    private static final int PRIVATE_DNS_MODE_ADGUARD = 5;
-
     private final Handler mHandler;
     private final ContentObserver mSettingsObserver;
     private final ConnectivityManager mConnectivityManager;
@@ -133,27 +129,13 @@ public class PrivateDnsPreferenceController extends BasePreferenceController
         switch (mode) {
             case PRIVATE_DNS_MODE_OFF:
                 return res.getString(R.string.private_dns_mode_off);
-            case PRIVATE_DNS_MODE_CLOUDFLARE:
-            case PRIVATE_DNS_MODE_ADGUARD:
             case PRIVATE_DNS_MODE_OPPORTUNISTIC:
                 return dnsesResolved ? res.getString(R.string.private_dns_mode_on)
                         : res.getString(R.string.private_dns_mode_opportunistic);
             case PRIVATE_DNS_MODE_PROVIDER_HOSTNAME:
-                if (!dnsesResolved) {
-                    return res.getString(R.string.private_dns_mode_provider_failure);
-                }
-                final String privateDnsHostname =
-                        ConnectivitySettingsManager.getPrivateDnsHostname(mContext);
-                final String cloudflareHostname =
-                        res.getString(R.string.private_dns_hostname_cloudflare);
-                final String adguardHostname =
-                        res.getString(R.string.private_dns_hostname_adguard);
-                if (privateDnsHostname.equals(cloudflareHostname)) {
-                    return res.getString(R.string.private_dns_mode_cloudflare);
-                } else if (privateDnsHostname.equals(adguardHostname)) {
-                    return res.getString(R.string.private_dns_mode_adguard);
-                }
-                return PrivateDnsModeDialogPreference.getHostnameFromSettings(cr);
+                return dnsesResolved
+                        ? PrivateDnsModeDialogPreference.getHostnameFromSettings(cr)
+                        : res.getString(R.string.private_dns_mode_provider_failure);
         }
         return "";
     }
